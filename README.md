9584b98d464926cfa02f5b0e60d897d69b4b88fb
Brendan Walker <brenny_walker@hotmail.com>
2018-01-28 10:56:46 -0800

    Configures linting, adds Flow type checking, tweaks some of the files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Phaser + ES6 + Webpack.
 #### A bootstrap project to create games with Phaser + ES6 + Webpack.
+### Flow-Integration branch
 
 ![Phaser+ES6+Webpack](https://raw.githubusercontent.com/lean/phaser-es6-webpack/master/assets/images/phaser-es6-webpack.jpg)
 
--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,14 +1,15 @@
-import Phaser from 'phaser'
-import WebFont from 'webfontloader'
+// @flow
+import Phaser from 'phaser-ce';
+import WebFont from 'webfontloader';
 
 export default class extends Phaser.State {
-  init () {
-    this.stage.backgroundColor = '#EDEEC9'
-    this.fontsReady = false
-    this.fontsLoaded = this.fontsLoaded.bind(this)
+  init() {
+    this.stage.backgroundColor = '#EDEEC9';
+    this.fontsReady = false;
+    this.fontsLoaded = this.fontsLoaded.bind(this);
   }
 
-  preload () {
+  preload() {
     WebFont.load({
       google: {
         families: ['Bangers']
@@ -16,20 +17,20 @@ export default class extends Phaser.State {
       active: this.fontsLoaded
     })
 
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
+    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' });
+    text.anchor.setTo(0.5, 0.5);
 
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('loaderBg', './assets/images/loader-bg.png');
+    this.load.image('loaderBar', './assets/images/loader-bar.png');
   }
 
-  render () {
+  render() {
     if (this.fontsReady) {
-      this.state.start('Splash')
+      this.state.start('Splash');
     }
   }
 
-  fontsLoaded () {
-    this.fontsReady = true
+  fontsLoaded() {
+    this.fontsReady = true;
   }
 }
